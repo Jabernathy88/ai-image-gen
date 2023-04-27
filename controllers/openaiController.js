d2b84e6d87e2b7ba388ce6708b4a4e68edeed1f3
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-04-27 15:12:54 -0400

    Successful API call, tested. Added image of bear to ./public folder

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIapi } = require("openai")
+const { Configuration, OpenAIApi } = require("openai")
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -8,15 +8,28 @@ const openai = new OpenAIApi(configuration)
 const generateImage = async (req, res) => {
     try {
         const response = await openai.createImage({
-            prompt: 'Polar bear on ice skates',
+            prompt: 'Brown bear Magic the Gathering', // next: user req params
             n: 1,
-            size: '512x512'
-        })        
+            size: '512x512' // TODO: refactor into a user-facing parameter
+        })
 
-        // TODO: START BACK HERE
+        const imageUrl = response.data.data[0].url
+        
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        })
+        
 
     } catch (error) {
-
+        if (error.response) {
+            console.log(error.response.status)
+            console.log(error.response.data)
+        }
+        res.status(400).json({
+            success: false,
+            message: 'Image could not be generated.'
+        })
     }
 
 }
